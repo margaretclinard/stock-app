3bde7c7188dfeb0db8590c63b703a0751cc69b53
Margaret Clinard <margaret.clinard@gmail.com>
2015-02-02 13:02:44 -0600

    updated index and main js

--- main.js
+++ main.js
@@ -5,5 +5,15 @@ $.getJSON(url, function (response) {
   console.log(response);
 });
 
+function getStock() {
+  var baseUrl = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=',
+      urlEnd = '&callback=?',
+      stockSymbol = getStockSymbol(),
+      url = baseUrl + stockSymbol + urlEnd;
+  return url;
+}
 
-
+function getStockSymbol() {
+  return document.getElementByClass('symbol').value;
+  //$
+}
