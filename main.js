95c3d3a6f0a0f14341f8ff937846a61a02995e9c
Margaret Clinard <margaret.clinard@gmail.com>
2015-02-02 12:31:18 -0600

    made url dynamic for input box

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-$.getJSON('http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=AAPL&callback=?', function (response) {
+var input = 'AAPL';
+var url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' + input + '&callback=?';
+
+$.getJSON(url, function (response) {
   console.log(response);
 });
+
+
